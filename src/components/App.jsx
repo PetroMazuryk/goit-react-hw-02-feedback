ad7082056030ed0172267c3ec541432b59defe76
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2023-03-01 22:55:45 +0200

    changed markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
+import React from 'react';
+
+import { FeedbackApp } from './FeedbackApp';
+import { GlobalStyle } from './GlobalStyle';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template good 33
+    <div>
+      <GlobalStyle />
+      <FeedbackApp />
     </div>
   );
 };
